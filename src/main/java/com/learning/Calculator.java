afe9d7a40a6dc70ede8d2e7d711e475225a1adf7
siva Kumar ravula <sivaravula@health.usf.edu>
2017-10-19 23:58:08 -0400

    initializing

---
+++ src/main/java/com/learning/Calculator.java
@@ -0,0 +1,11 @@
+package com.learning;
+
+public class Calculator {
+	public int add(int a, int b) {
+        return a+b;
+	}
+	public int mul(int a, int b) {
+        return a*b;
+	}
+
+}
---
+++ src/test/java/com/learning/CalculatorTest.java
@@ -0,0 +1,19 @@
+package com.learning;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CalculatorTest {
+
+	@Test public void testAdd() {
+        Calculator calc = new Calculator();
+        
+        assertEquals(7,calc.add(3,4));
+    }
+	@Test public void testmul() {
+        Calculator calc = new Calculator();
+        
+        assertEquals(12,calc.mul(3,4));
+    }
+}
